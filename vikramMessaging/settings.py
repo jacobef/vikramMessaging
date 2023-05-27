744ba64b2c00646fbe84ac49a1ff467d0c53d702
jacobef <jacobef2@gmail.com>
2023-05-26 15:28:11 -1000

    add under construction notice

--- vikramMessaging/settings.py
+++ vikramMessaging/settings.py
@@ -121,7 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = "static/"
+# STATIC_ROOT = "static/"
 STATICFILES_DIRS = ["static/"]
 
 MEDIA_URL = "media/"
