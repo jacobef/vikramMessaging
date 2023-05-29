8595afc214a32d339574398114c599c113d40df5
jacobef <jacobef2@gmail.com>
2023-05-28 15:07:36 -1000

    you have n unread chats

--- vikramMessaging/views.py
+++ vikramMessaging/views.py
@@ -2,9 +2,16 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render, redirect
 
+from messaging.models import GroupMessage, MessagingGroup
+
+
 @login_required
 def home(request):
-    return render(request, "home.html")
+    return render(request, "home.html",
+                  {"num_unread_chats": MessagingGroup.objects
+                  .filter(members=request.user)
+                  .exclude(read_list=request.user)
+                  .count()})
 
 def create_account(request):
     if request.method == "GET":
